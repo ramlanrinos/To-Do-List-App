d7c0cf6a3a7b328ed6576a1ca935b703fe8f3e50
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-01 17:12:34 +0530

    create functions for home and each tasks in views.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+
+@login_required
+def home(request):
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'todo/home.html', {'tasks': tasks})
+
+@login_required
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/add_task.html', {'form': form})
+
+@login_required
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/update_task.html', {'form': form, 'task': task})
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+    return render(request, 'todo/delete_task.html', {'task': task})
+
+@login_required
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.completed = not task.completed
+    task.save()
+    return redirect('home')
